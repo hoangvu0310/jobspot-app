fedab101a5673bce771e454913103019107059df
hoangvu0310 <hoangvu031003@gmail.com>
2026-03-03 14:56:06 +0700

    add fonts and colors

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,37 @@ import '@/i18n'
 
 import { Slot } from 'expo-router'
 import { ThemeProvider } from '@/context'
+import {
+	useFonts,
+	JosefinSans_400Regular,
+	JosefinSans_500Medium,
+	JosefinSans_600SemiBold,
+	JosefinSans_700Bold,
+} from '@expo-google-fonts/josefin-sans'
+import { useEffect } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
+	const [loaded, error] = useFonts({
+		JosefinSans_400Regular,
+		JosefinSans_500Medium,
+		JosefinSans_600SemiBold,
+		JosefinSans_700Bold,
+	})
+
+	useEffect(() => {
+		if (error) {
+			console.error(error)
+		}
+
+		if (loaded) {
+			console.info('Fonts loaded')
+			SplashScreen.hideAsync()
+		}
+	}, [error, loaded])
+
 	return (
 		<ThemeProvider>
 			<Slot />
